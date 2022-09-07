1b447be72f575bb8f796c73b8e8c0365ff05eaa2
Yuri Loayza <lllyuro20@gmail.com>
2022-09-07 15:40:04 +0100

    commit

--- src/App.java
+++ src/App.java
@@ -1,130 +1,110 @@
 import java.io.File;
-import org.w3c.dom.Element;
 import java.time.LocalDate;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.apache.xpath.CachedXPathAPI;
-import javax.xml.parsers.DocumentBuilder;
 import java.time.format.DateTimeFormatter;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.TransformerException;
+import org.apache.xpath.CachedXPathAPI;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-     
+
+  public static void main(String[] args) throws Exception {
     DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
     DocumentBuilder db = dbf.newDocumentBuilder();
     org.w3c.dom.Document doc = db.parse(new File("Title.xml"));
     expiredDoc(doc);
-    }
+  }
 
-public static Document expiredDoc(Document doc) {
-
+  public static Document expiredDoc(Document doc) {
     //sysdate exctraction
+    int j = 1;
     int i = 0;
     LocalDate sysDate = null;
     LocalDate endDate = null;
     LocalDate startDate = null;
-    CharSequence sEndDate = null;
-    CharSequence sStartDate = null;
-    DateTimeFormatter formatter = null;
     CachedXPathAPI v = new CachedXPathAPI();
 
     //hour and date vars
     NodeList endHourValues = null;
     NodeList endDateValues = null;
-    Element endDateElement = null;    
-    Element endHourElement = null;
     NodeList startDateValues = null;
     NodeList startHourValues = null;
-    Element startDateElement = null;    
-    Element startHourElement = null;
     String sEndHourValuesPath = "/content/group/definition/list/value/schedulation/list/value/window/text[@name='endHour']/value";
     String sStartHourValues = "/content/group/definition/list/value/schedulation/list/value/window/text[@name='startHour']/value";
     String endDateValuesPath = "/content/group/definition/list/value/schedulation/list/value/window/date[@name='endDate']/value";
     String startDateValuesPath = "/content/group/definition/list/value/schedulation/list/value/window/date[@name='startDate']/value";
 
     //utilized for the removal of multiple events collision
-    Element newNode = null;
-    NodeList wholeList = null;
-    NodeList removaList = null;
-    NodeList newNodesList = null;
-    String str2 = "/content/group/definition/list/value/schedulation/list[@name='fruitionWindows']";
-    String str1 = "/content/group/definition/list/value/schedulation/list[@name='fruitionWindows']/value";
-    String str = "/content/group/definition/list/value/schedulation/list[@name='fruitionWindows']/value/window[@id='0']";
+    String schedulationPath = "/content/group/definition/list/value/schedulation";
+    String windowPath = "list[@name='fruitionWindows']/value/window";
+    NodeList windowList = null;
 
-        try {
+    try {
+      NodeList schedulationList = v.selectNodeList(doc, schedulationPath);
 
-        newNodesList = v.selectNodeList(doc, str);
-        removaList = v.selectNodeList(doc, str1);
-        wholeList = v.selectNodeList(doc, str2);
+      //cycle on schedulation lenght
+      for (i = 0; i < schedulationList.getLength(); i++) {
+        windowList = v.selectNodeList(schedulationList.item(i), windowPath);
 
-        } catch (TransformerException e) {
-            e.printStackTrace();
+        //check if more than 1 window node is present
+        if (windowList.getLength() > 1) {
+          //for every extra window node remove it
+          for (j = 1; j < windowList.getLength(); j++) {
+            windowList.item(j).getParentNode().removeChild(windowList.item(j));
+          }
         }
+      }
 
-        for (i = 0; i < newNodesList.getLength(); i++) {
-            
-        newNode = (Element)  newNodesList.item(i);
-        wholeList.item(i).removeChild(removaList.item(i));
-        wholeList.item(i).appendChild(newNode);
-        
-        }
+      //data/hour values
 
-        //data/hour values
-        try {
+      startDateValues = v.selectNodeList(doc, startDateValuesPath);
+      startHourValues = v.selectNodeList(doc, sStartHourValues);
+      endDateValues = v.selectNodeList(doc, endDateValuesPath);
+      endHourValues = v.selectNodeList(doc, sEndHourValuesPath);
+      sysDate = LocalDate.now();
+      sysDate = sysDate.minusDays(1);
 
-            startDateValues = v.selectNodeList(doc, startDateValuesPath);
-            startHourValues = v.selectNodeList(doc, sStartHourValues);
-            endDateValues = v.selectNodeList(doc, endDateValuesPath);
-            endHourValues = v.selectNodeList(doc, sEndHourValuesPath);
-            sysDate = LocalDate.now();
-            sysDate = sysDate.minusDays(1);
-    
-    
-        } catch (TransformerException e1) {
-            e1.printStackTrace();
-        }
-
-        //main loop
-        for (i=0; i < newNodesList.getLength(); i++) {
-
-        sStartDate = startDateValues.item(i).getTextContent();
-        sEndDate = endDateValues.item(i).getTextContent();
-        formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
+      //main loop
+      for (i = 0; i < windowList.getLength(); i++) {
+        CharSequence sStartDate = startDateValues.item(i).getTextContent();
+        CharSequence sEndDate = endDateValues.item(i).getTextContent();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
         startDate = LocalDate.parse(sStartDate, formatter).minusDays(1);
         endDate = LocalDate.parse(sEndDate, formatter).minusDays(1);
 
         //if end date > sysdate --> case -1
         switch (endDate.compareTo(sysDate)) {
-            case -1:
-
-            endDateElement = (Element) endDateValues.item(i);
+          case -1:
+            Element endDateElement = (Element) endDateValues.item(i);
             endDateElement.getFirstChild().setNodeValue(sysDate.toString());
-            endHourElement = (Element) endHourValues.item(i);
+            Element endHourElement = (Element) endHourValues.item(i);
             endHourElement.getFirstChild().setNodeValue("23:59");
             break;
-
-            default:
+          default:
             break;
         }
 
         //if start date > sysdate --> case -1
         switch (startDate.compareTo(sysDate)) {
-            case -1:
-            
-            startDateElement = (Element) startDateValues.item(i);
+          case -1:
+            Element startDateElement = (Element) startDateValues.item(i);
             startDateElement.getFirstChild().setNodeValue(sysDate.toString());
 
-            startHourElement = (Element) startHourValues.item(i);
+            Element startHourElement = (Element) startHourValues.item(i);
             startHourElement.getFirstChild().setNodeValue("00:00");
 
             break;
-            default:
+          default:
             //nothing changed
             break;
         }
-    } 
-        return doc;
+      }
+    } catch (TransformerException e1) {
+      e1.printStackTrace();
     }
+    return doc;
+  }
 }
